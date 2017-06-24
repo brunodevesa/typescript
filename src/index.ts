96e0932b5429797f6b36cb684f816ef866a81e9a
CRITICAL\bmdevesa <brunodevesa@criticalmanufacturing.com>
2017-06-24 18:02:34 +0100

    code cleaning

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,10 @@
-import { Animals } from './jsonData/Animals'
-import { Animal } from './jsonData/Animal'
-import { FilterClass } from './filter/Filter';
+
 
 console.log("Typescript Basics");
-
-// filter, map and reduce exercices
-
-let myFilterClass = new FilterClass();
-let animals = new Animals();
-let myAnimals: Animal[] = animals.animals;
-
-let myAnimalsAboveAge4  = myFilterClass.animalsAboveAge4;
-console.log(myAnimalsAboveAge4)
-
-let myAnimalsAbove = myFilterClass.animalsAboveSomeAge(myAnimals , 1 )
-console.log('animals above some age :')
-console.log(myAnimalsAbove)
+console.log('Go to specific folder and run :')
+console.log('/folder/tsc index.ts')
+console.log(' for transpile and after..')
+console.log('/folder/node index.js')
 
 
-let animalsNameStartWithLetterK = myFilterClass.animalsNameStartWithLetterK(myAnimals);
 
-
-console.log('animals name starting with letter k :');
-console.log('....................................')
-console.log(animalsNameStartWithLetterK)
-
-
-let letter = 'b'
-let animalsNameStartWithSomeLetter = myFilterClass.animalsNameStartWithSomeLetter(myAnimals, letter) ;
-console.log('animals name starting with letter  : ' + letter);
-console.log('....................................')
-console.log(animalsNameStartWithSomeLetter)
-
-
-let ageArrayOfAllAnimals= myFilterClass.ageArrayOfAllAnimals(myAnimals);
-
-console.log('age array of all animals :')
-console.log(ageArrayOfAllAnimals);
-
-let averageAgeOfAllAnimals = myFilterClass.averageAge(ageArrayOfAllAnimals)
-console.log('average age of all animals :')
-console.log(averageAgeOfAllAnimals);
